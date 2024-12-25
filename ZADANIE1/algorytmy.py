14b84e2d2ec5f156d656113e6a56b6e6d33f3d06
xyzqwer <112662623+matipt2@users.noreply.github.com>
2024-12-25 13:54:48 +0000

    tasks done

--- ZADANIE1/algorytmy.py
+++ ZADANIE1/algorytmy.py
@@ -52,32 +52,118 @@ def shell_sort(array: MonitorowanaTablica):
 
 
 
-def merge_sort(array: MonitorowanaTablica, left=None, right=None):
-# twoj kod
-    pass
+def merge_sort(array: MonitorowanaTablica, left=0, right=None):
+    if right is None:
+        right = len(array) - 1
 
+    if right - left <= 10:
+        for i in range(left + 1, right + 1):
+            key = array[i]
+            j = i - 1
+            while j >= left and array[j] > key:
+                array[j + 1] = array[j]
+                j -= 1
+            array[j + 1] = key
+        return
 
-def merge(array: MonitorowanaTablica, left, middle, right):
-    """Merges two sorted subarrays."""
-    # twoj kod, moze sie przydac
-    pass
+    if left < right:
+        mid = (left + right) // 2
+        merge_sort(array, left, mid)
+        merge_sort(array, mid + 1, right)
 
+        if array[mid] <= array[mid + 1]:
+            return
 
-def quick_sort(array: MonitorowanaTablica, left=None, right=None):
-    """Performs quick sort on the given array."""
-    # twoj kod
-    pass
+        merge(array, left, mid, right)
 
-
-def partition(array: MonitorowanaTablica, left, right):
-    """Partitions the array into two parts."""
-    # twoj kod, moze sie przydac
-    pass
-
+def merge(array: MonitorowanaTablica, left, mid, right):
+    left_size = mid - left + 1
+    temp = [] 
+    
+    for i in range(left_size):
+        temp.append(array[left + i])
+    
+    i = 0 
+    j = mid + 1
+    k = left 
+    
+    while i < left_size and j <= right:
+        if temp[i] <= array[j]:
+            array[k] = temp[i]
+            i += 1
+        else:
+            array[k] = array[j]
+            j += 1
+        k += 1
+    
+    while i < left_size:
+        array[k] = temp[i]
+        i += 1
+        k += 1
 
 def tim_sort(array: MonitorowanaTablica):
-# twoj kod
-    pass
+    min_run = 32
+    n = len(array)
+
+    for start in range(0, n, min_run):
+        end = min(start + min_run - 1, n - 1)
+        insertion_sort(array, start, end)
+
+    size = min_run
+    while size < n:
+        for left in range(0, n, 2 * size):
+            mid = min(left + size - 1, n - 1)
+            right = min(left + 2 * size - 1, n - 1)
+            if mid < right:
+                temp = []
+                i, j = left, mid + 1
+                
+                while i <= mid and j <= right:
+                    if array[i] <= array[j]:
+                        temp.append(array[i])
+                        i += 1
+                    else:
+                        temp.append(array[j])
+                        j += 1
+                
+                while i <= mid:
+                    temp.append(array[i])
+                    i += 1
+                
+                while j <= right:
+                    temp.append(array[j])
+                    j += 1
+                
+                for i, val in enumerate(temp):
+                    array[left + i] = val
+                    
+        size *= 2
+
+
+
+def quick_sort(array: MonitorowanaTablica, left=0, right=None):
+    if right is None:
+        right = len(array) - 1
+
+    if left < right:
+        pivot_index = partition(array, left, right)
+        quick_sort(array, left, pivot_index - 1)
+        quick_sort(array, pivot_index + 1, right)
+
+def partition(array: MonitorowanaTablica, left, right):
+    pivot = array[right]
+    i = left - 1
+
+    for j in range(left, right):
+        if array[j] <= pivot:
+            i += 1
+            array[i], array[j] = array[j], array[i]
+
+    array[i + 1], array[right] = array[right], array[i + 1]
+    return i + 1
+
+
+
 
 
 
@@ -85,7 +171,7 @@ algorytmy = [
     (insertion_sort, "Insertion Sort"),
     (bubble_sort, "Bubble Sort"),
     (shell_sort, "Shell Sort"),
-    # (merge_sort, "Merge Sort"),
-    # (quick_sort, "Quick Sort"),
-    # (tim_sort, "Tim Sort"),
+    (merge_sort, "Merge Sort"),
+    (quick_sort, "Quick Sort"),
+    (tim_sort, "Tim Sort"),
 ]
--- ZADANIE2/maximize_it.py
+++ ZADANIE2/maximize_it.py
@@ -1,12 +1,11 @@
 from itertools import product
 
-
-
 def maximize_expression(K, M, lists):
-    # twoj kod tutaj
-    pass 
-
-
+    max_s = 0
+    for combination in product(*lists):
+        s = sum(x**2 for x in combination) % M
+        max_s = max(max_s, s)
+    return max_s
 
 if __name__ == "__main__":
     K, M = map(int, input().rstrip().split())
